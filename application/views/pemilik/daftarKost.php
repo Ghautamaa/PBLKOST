b55ac62111cab6b20904eaa39ee4c5477b90449f
MartabakEnak <arakianjuven@gmail.com>
2024-01-02 13:44:08 +0800

    tombol kembali

--- application/views/pemilik/daftarKost.php
+++ application/views/pemilik/daftarKost.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Daftar Kost</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -47,7 +47,13 @@
       <p class="mb-1"><label for="Deskripsi">Deskripsi :</label></p>
       <textarea name="deskripsi" id="deskripsi" cols="100" rows="10" required></textarea>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="d-flex  justify-content-start">
+      <a class="btn btn-danger col-2" href="<?php echo base_url('ctampilan/dashboard_pemilik'); ?>">Back</a>
+    </div>
+    <div class="d-flex  justify-content-end">
+      <button typ e="submit" class="btn btn-primary col-2">Submit</button>
+    </div>
+    
   </form>
 </div>
 
